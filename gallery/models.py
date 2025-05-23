531e8cb537898596eb04d31145ab7f9ddc5d3275
perpetuumddt <perpetuumddt@gmail.com>
2025-05-23 09:22:14 +0300

    Added Image model

--- gallery/models.py
+++ gallery/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -9,3 +10,17 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
+
+class Image(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/')
+    categories = models.ManyToManyField(Category, related_name='images')
+    created_date = models.DateField(default=timezone.now)
+    age_limit = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Image'
+        verbose_name_plural = 'Images'
