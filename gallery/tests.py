3b75f2e9d0fa90db038da68f6c9978fed6871f7e
perpetuumddt <perpetuumddt@gmail.com>
2025-05-23 09:37:44 +0300

    Added pytest file

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.utils import timezone
+from .models import Category, Image
+import os
 
-# Create your tests here.
+#testing category model
+class CategoryModelTest(TestCase):
+    #setting up test category
+    def setUp(self):
+        self.category = Category.objects.create(name="Test Category")
+
+
+#testing image model
+class ImageModelTest(TestCase):
+    #setting up test image
+    def setUp(self):
+        # Create test category
+        self.category = Category.objects.create(name="Test Category")
+        
+        # Create test image file
+        self.test_image = SimpleUploadedFile(
+            name='test_image.jpg',
+            content=b'',
+            content_type='image/jpeg'
+        )
+        
+        # Create test image object
+        self.image = Image.objects.create(
+            title="Test Image",
+            image=self.test_image,
+            created_date=timezone.now().date(),
+            age_limit=18
+        )
+        self.image.categories.add(self.category)
+
